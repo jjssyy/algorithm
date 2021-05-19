06551fb2bdcd9cf5a30fca8ae962476e14d46722
jjssyy <84180020+jjssyy@users.noreply.github.com>
2021-05-19 17:16:19 +0900

    Create 1309번 동물원.java

---
+++ BOJ/1309번 동물원.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class _1309_동물원 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[][] dp = new int[N][3];
+		
+		dp[0][0] = 1;
+		dp[0][1] = 1;
+		dp[0][2] = 1;
+		
+		for(int i=1; i<N; i++) {
+			dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2])%9901;
+			dp[i][1] = (dp[i-1][0] + dp[i-1][2])%9901;
+			dp[i][2] = (dp[i-1][0] + dp[i-1][1])%9901;
+		}
+		
+		int ans = (dp[N-1][0] + dp[N-1][1] + dp[N-1][2])%9901;
+		System.out.println(ans);
+	}
+}
