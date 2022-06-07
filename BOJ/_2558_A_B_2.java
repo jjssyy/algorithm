ad78f030693812b89627556be0c3dcd3fbbb1bd8
nnaoww <nnaoww@naver.com>
2022-06-07 15:46:30 +0900

    2558

---
+++ BOJ/_2558_A_B_2.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _2558_A_B_2 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a+b);
+        
+    }
+}
