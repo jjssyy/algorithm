7091765e513019a484f5a07ffbd76a513127d2cd
nnaoww <nnaoww@naver.com>
2022-06-07 23:37:42 +0900

    11022

---
+++ BOJ/_11022_A_B_8.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11022_A_B_8 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int sum = a+b;
+
+            System.out.println("Case #"+i+": "+a+" + "+b+" = "+sum);
+        }
+    }
+}
