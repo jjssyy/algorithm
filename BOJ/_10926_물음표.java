d7aebbd424a2615821c9aafb0d448407a270e2c1
nnaoww <nnaoww@naver.com>
2022-06-08 22:44:28 +0900

    10926

---
+++ BOJ/_10926_물음표.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10926_물음표 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine()+"??!");
+    }
+}
