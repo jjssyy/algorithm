cbf6da9fbea94653dc211c863bae037d832b5dd8
nnaoww <nnaoww@naver.com>
2022-06-08 20:34:47 +0900

    18108

---
+++ BOJ/_18108_1998년생인_내가_태국에서는_2541년생.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _18108_1998년생인_내가_태국에서는_2541년생 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine())-543);
+    }
+}
