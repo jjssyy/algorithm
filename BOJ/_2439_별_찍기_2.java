f8b84e1fc037c9d7bf12d850d46b9437090db7c3
nnaoww <nnaoww@naver.com>
2022-06-13 18:58:42 +0900

    2439

---
+++ BOJ/_2439_별_찍기_2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2439_별_찍기_2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
