2c7d1bcbafea5997354f7e4c1cfb47a348e684b3
nnaoww <nnaoww@naver.com>
2022-06-17 22:35:36 +0900

    3052

---
+++ BOJ/_3052_나머지.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _3052_나머지 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[42];
+        int cnt=0;
+
+        for(int i=0; i<10; i++){
+            arr[Integer.parseInt(br.readLine())%42]++;
+        }
+        for(int i=0; i<42; i++){
+            if(arr[i]>0){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
