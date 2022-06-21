fb1861f1acdf4a79a8ebba59752975b148622c1b
nnaoww <nnaoww@naver.com>
2022-06-21 23:59:52 +0900

    15596

--- BOJ/_15596_정수_N개의_합.java
+++ BOJ/_15596_정수_N개의_합.java
@@ -1,5 +1,11 @@
 public class _15596_정수_N개의_합 {
-    public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
+
+    long sum(int[] a){
+        long sum =0;
+        for(int i=0; i<a.length; i++){
+            sum+=a[i];
+        }
+
+        return sum;
     }
 }
