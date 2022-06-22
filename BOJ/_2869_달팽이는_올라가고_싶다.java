ec41cac411fbe02d16e5b832ce26e807dddc1dcf
nnaoww <nnaoww@naver.com>
2022-06-22 17:33:59 +0900

    2869

---
+++ BOJ/_2869_달팽이는_올라가고_싶다.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2869_달팽이는_올라가고_싶다 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V-A)/(A-B)+1;
+
+        if((V-A)%(A-B)!=0){
+            day++;
+        }
+        System.out.println(day);
+    }
+}
