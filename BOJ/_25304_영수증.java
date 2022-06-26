6e26286bf53197c38ef4cf15a5eed25c7fe0a822
nnaoww <nnaoww@naver.com>
2022-06-26 22:51:43 +0900

    25304

---
+++ BOJ/_25304_영수증.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _25304_영수증 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum =0;
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sum += Integer.parseInt(st.nextToken())*Integer.parseInt(st.nextToken());
+        }
+
+        if(X==sum){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
