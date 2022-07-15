a5740bf11f1a0f6ccfd64712932865bf37492937
nnaoww <nnaoww@naver.com>
2022-07-16 01:09:50 +0900

    string

--- Programmers/_lv1_핸드폰_번호_가리기.java
+++ Programmers/_lv1_핸드폰_번호_가리기.java
@@ -5,17 +5,14 @@ public class _lv1_핸드폰_번호_가리기 {
     }
     static public String solution(String phone_number) {
         String answer = "";
-
-        StringBuilder sb = new StringBuilder();
         
         for(int i=0; i<phone_number.length()-4; i++){
-            sb.append("*");
+            answer += "*";
         }
         for(int i=phone_number.length()-4; i<phone_number.length(); i++){
-            sb.append(phone_number.charAt(i));
+            answer += phone_number.charAt(i);
         }
 
-        answer = sb.toString();
         System.out.println(answer);
         return answer;
     }
