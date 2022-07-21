8b57984579f04f201530beeca27eabb04a3abc61
nnaoww <nnaoww@naver.com>
2022-07-21 17:20:07 +0900

    sol

---
+++ Programmers/_lv1_2016년.java
@@ -0,0 +1,24 @@
+public class _lv1_2016년 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 7;
+        solution(a, b);
+    }
+    static public String solution(int a, int b) {
+        String[] day = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int tmp = b;
+        for(int i=0; i<a-1; i++){
+            tmp += month[i];
+        }
+
+        if(tmp%7 ==0){
+            return "FRI";
+        }
+        String answer = day[tmp%7-1];
+
+        System.out.println(answer);
+        return answer;
+    }
+}
