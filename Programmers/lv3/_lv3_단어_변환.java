6441ce43473b40775196b76376e848a527566c03
nnaoww <nnaoww@naver.com>
2022-07-23 06:20:45 +0900

    .

---
+++ Programmers/lv3/_lv3_단어_변환.java
@@ -0,0 +1,13 @@
+public class _lv3_단어_변환 {
+    public static void main(String[] args) {
+        String begin = "hit";
+        String target = "cog";
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+        
+        solution(begin, target, words);
+    }
+    public static int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
+    }
+}
