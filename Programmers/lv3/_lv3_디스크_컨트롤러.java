f1cca425a561d40836fe54259081357a5ced04c0
nnaoww <nnaoww@naver.com>
2022-07-26 11:49:30 +0900

    .

---
+++ Programmers/lv3/_lv3_디스크_컨트롤러.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class _lv3_디스크_컨트롤러 {
+    public static void main(String[] args) {
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+        solution(jobs);
+    }
+    static public int solution(int[][] jobs) {
+        int answer = 0;
+        return answer;
+    }
+    static class Job{
+        int start;
+        int cost;
+        
+        public Job(int start, int cost){
+            this.start = start;
+            this.cost = cost;
+        }
+    }
+}
