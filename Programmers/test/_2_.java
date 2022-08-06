026bbcc92226174ba9b054c68b7614a7d36727d4
nnaoww <nnaoww@naver.com>
2022-08-06 14:43:37 +0900

    .

---
+++ Programmers/test/_2_.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class _2_ {
+    public static void main(String[] args) {
+        int[] levels = {1,2,3,4};
+        solution(levels);
+    }
+    static public int solution(int[] levels) {
+        int answer = 0;
+        int sum =0;
+        Arrays.sort(levels);
+
+        for(int i=0; i<levels.length; i++){
+            sum+=levels[i];
+        }
+        double standard = sum*0.25;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        double s=0;
+        for(int i=levels.length-1; i>=0; i--){
+            
+            if(sum/levels[i]+sum%levels[i]*0.1+s<=standard){
+                s+=sum/levels[i]+sum%levels[i]*0.1;
+                list.add(levels[i]);
+            }
+        }
+        Collections.sort(list);
+        answer = list.get(0);
+        return answer;
+    }
+}
